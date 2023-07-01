9c629a0eee9b7c515b516d3abf0fdb1cd042454f
Francisco Zorat <francisco.zorat@g4gaming.com.ar>
2023-07-01 00:31:20 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { api } from './api/api.js';
 import { navbarItem } from './components/navbar.js';
+import { mainFooter } from './components/footer.js';
 
 const navItems = [
     {
@@ -27,11 +28,21 @@ const navItems = [
     },
 ];
 
-const navbar = document.getElementById('navbar');
-
 /* fabricating navbar "on demand" */
+const navbar = document.getElementById('navbar');
 navbar.innerHTML = navItems
     .map(({ name, href, grow }) => {
         return navbarItem({ name, href, grow });
     })
     .join('');
+
+/* fabricating footer "on demand" */
+const footer = document.getElementById('main-footer');
+footer.innerHTML = mainFooter();
+
+/* only for index page */
+const mainContent = document.getElementById('main-content');
+if (!!mainContent) {
+    const carousel = document.getElementById('carousel');
+    carousel.innerHTML = 'CAROUSEL';
+}
