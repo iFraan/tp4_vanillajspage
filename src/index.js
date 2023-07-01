b25da3318cdbe2c3a9e2b26d9a1c691bb9cea4b9
Francisco Zorat <francisco.zorat@g4gaming.com.ar>
2023-07-01 00:09:24 -0300

    navbar items on script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import { api } from './api/api';
+import { api } from './api/api.js';
+import { navbarItem } from './components/navbar.js';
+
+const navItems = [
+    {
+        name: 'Home',
+        href: 'index.html',
+    },
+    {
+        name: 'Discover',
+        href: 'discover.html',
+    },
+    {
+        name: 'Peliculas',
+        href: 'movies.html',
+    },
+    {
+        name: 'Series',
+        href: 'tv.html',
+    },
+];
+
+const navbar = document.getElementById('navbar');
+
+/* fabricating navbar "on demand" */
+navbar.innerHTML = navItems
+    .map(({ name, href }) => {
+        return navbarItem({ name, href });
+    })
+    .join('');
