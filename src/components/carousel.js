7ec7c8a98f121b32e31a449195fac80624cd3e67
Francisco Zorat <francisco.zorat@g4gaming.com.ar>
2023-07-01 01:25:27 -0300

    year in carousel item

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -6,6 +6,7 @@ export const carouselItem = (item) => {
         <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black to-black/0 rounded-2xl"></div>
         <div class="absolute bottom-16 left-12 text-5xl font-bold text-white z-20">${item.title}</div>
         <div class="absolute bottom-8 left-12 text-xl text-white z-20">${item.original_title}</div>
+        <div class="absolute bottom-8 right-12 text-2xl font-bold text-slate-300 z-20">${new Date(item.release_date).getFullYear()}</div>
     </div>
     `;
 };
