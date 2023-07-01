9b8cd748a7d78c32c67820707c5ccec649e8f9a9
Francisco Zorat <francisco.zorat@g4gaming.com.ar>
2023-07-01 01:22:19 -0300

    carousel items

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,7 +1,11 @@
 export const carouselItem = (item) => {
+    console.log(item);
     return `
     <div class="carousel-item ${item.active && 'active'} h-full w-full">
-        <img class="object-cover h-full w-full" src="${item.backdrop}" alt="${item.title}" />
+        <img class="absolute object-cover object-[center_top] h-full w-full rounded-2xl" src="${item.backdrop}" alt="${item.title}" />
+        <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black to-black/0 rounded-2xl"></div>
+        <div class="absolute bottom-16 left-12 text-5xl font-bold text-white z-20">${item.title}</div>
+        <div class="absolute bottom-8 left-12 text-xl text-white z-20">${item.original_title}</div>
     </div>
     `;
 };
