49d0841168aefd571594cd1665675346fb3bf829
Francisco Zorat <francisco.zorat@g4gaming.com.ar>
2023-07-01 00:16:43 -0300

    nav item

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,17 @@
 export const navbarItem = ({ name, href }) => {
     return `
-    <div class="h-10 w-10 bg-white">
-    </div>
+    <a class="
+        transition-all 
+        py-1.5 px-5 
+        rounded-md 
+        bg-slate-500/50 hover:bg-slate-600/50
+        scale-100 hover:scale-105
+        text-white
+        font-bold
+    "
+    href={${href}}
+    >
+        ${name}
+    </a>
     `;
 };
