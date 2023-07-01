a2ca42b3f8bc805a39f9bc925e43c95bb9bc0d3d
Francisco Zorat <francisco.zorat@g4gaming.com.ar>
2023-07-01 03:46:10 -0300

    maps year in every method

--- src/api/api.js
+++ src/api/api.js
@@ -2,14 +2,14 @@ import DATA from './data.js';
 
 export const api = {
     getMovies: () => {
-        return DATA.filter((item) => item.type === 'MOVIE');
+        return DATA.filter((item) => item.type === 'MOVIE').map((item) => ({ ...item, year: new Date(item.release_date).getFullYear() }));
     },
     getTv: () => {
-        return DATA.filter((item) => item.type === 'TV');
+        return DATA.filter((item) => item.type === 'TV').map((item) => ({ ...item, year: new Date(item.release_date).getFullYear() }));
     },
     getAll: ({ limit = 10, random = false }) => {
         const data = [...DATA];
         if (random) data.sort(() => Math.random() - 0.5);
-        return data.slice(0, limit);
+        return data.slice(0, limit).map((item) => ({ ...item, year: new Date(item.release_date).getFullYear() }));
     },
 };
